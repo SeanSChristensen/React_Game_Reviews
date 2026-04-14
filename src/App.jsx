ff45f6614a01e5ded30888ccbabb26f2a8846bb3
Sean <seanshouchristensen@gmail.com>
2026-04-14 14:29:46 +1000

    Adding basic routing for review page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
 
 function About() {
     return <h1>About Page</h1>;
@@ -9,12 +9,18 @@ function Home() {
     return <h1>Home</h1>;
 }
 
+function ReviewPage() {
+    const { gameName } = useParams();
+    return <h1>{gameName}</h1>;
+}
+
 
 export default function App() {
     return (
         <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/Review/:gameName" element={<ReviewPage />} />
         </Routes>
     );
 }
