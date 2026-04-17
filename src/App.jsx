ade75731ec8fef3f7b8348e1ed200b9d945293f6
Sean <seanshouchristensen@gmail.com>
2026-04-17 23:42:19 +1000

    Improved search page

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,13 @@ function Search() {
 
     return (
         <Layout>
-            <button onClick={() => { navigate("/Review/" + input); }}>Search</button>
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
+            <div class="input-group mb-3 w-50 position-absolute top-50 start-50 translate-middle">
+                <input type="text" class="form-control" placeholder="Game Name" aria-label="Recipient's username" aria-describedby="basic-addon2" value={input}
+                    onChange={(e) => setInput(e.target.value)} />
+                    <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" id="customSearchButton" type="button" onClick={() => { navigate("/Review/" + input); }}>Search</button>
+                    </div>
+            </div>
         </Layout>
     )
 }
