46277046b92edb279f86eae8a2c9861eb109c51b
Sean <seanshouchristensen@gmail.com>
2026-05-16 21:43:17 +1000

    Refactored games list page loading list state. Removed hook storing loading state as its not required and the game list value can be used instead

--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -3,19 +3,12 @@ import React, { useState, useEffect } from 'react';
 
 export default function List() {
     const [gameList, setGameList] = useState(null);
-    const [gameListLoadingStatus, setGameListLoadingStatus] = useState("loading");
 
     useEffect(() => {
         fetch(`http://localhost:3000/api/gameList`)
             .then(response => response.json())
             .then(json => {
-                if (json.status == "error") {
-                    setGameListLoadingStatus("error")
-                }
-                else {
-                    setGameList(json)
-                    setGameListLoadingStatus("success")
-                }
+                setGameList(json)
             })
     })
 
@@ -24,14 +17,14 @@ export default function List() {
             <div className="gamesList">
                 <h2>Games List</h2>
                 <p>List of current game pages</p>
-                {gameListLoadingStatus == "error loading game list please contact system administrators"
-                    ? (<p>Error</p>)
-                    : gameListLoadingStatus == "loading"
-                        ? (<div class="text-center">
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only"></span>
-                            </div>
-                        </div>)
+                {gameList == null
+                    ? (<div class="text-center">
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only"></span>
+                        </div>
+                    </div>)
+                    : gameList.status == "error"
+                        ? (<p>Error</p>)
                         : <ul>{gameList.data.map((gameName) => (<li><a href={`http://localhost:5173/Review/${gameName.name}`}>{gameName.name}</a></li>))}
                         </ul>}
 
