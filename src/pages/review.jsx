c7e5feb9a334049b81d4c6d60277f75f31d5f6ad
Sean <seanshouchristensen@gmail.com>
2026-05-27 10:45:12 +1000

    separated game information into its own component

--- src/pages/review.jsx
+++ src/pages/review.jsx
@@ -28,6 +28,60 @@ async function ApiFetchHandler(url, requestHeaders, requestBody) {
     }
 }
 
+function GameInfoComponent(props) {
+    let content;
+
+    if (props.gameInfo.loading) content =
+
+            <div className="text-center gameInfoLoadingSpinner">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only"></span>
+                </div>
+
+        </div>;
+    else if (props.gameInfo.error) {
+        content =
+            <div>
+                <p>{props.gameInfo.error}</p>
+            </div>
+    }
+    else {
+        const timestamp = props.gameInfo.data.release_date;
+        const formattedDate = new Date(timestamp).toLocaleString("en-US",
+            {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+            })
+
+        content =
+        <>                <h1 className='gameTitle'>{props.gameName}</h1>
+                <div className="gameAverageRating">
+                    <h3>Average Rating</h3>
+                    {[...Array(5)].map((star, index, rating) => {
+                        index += 1;
+                        rating = 3;
+                        return (
+                            <FaStar
+                                key={index}
+                                size={30}
+                                className={index <= (rating) ? 'on' : 'off'}
+                                color={index <= (rating) ? '#ffd700' : '#e4e5e9'} />
+                        );
+                    })}</div>
+                <div className='gameInformation'>
+                    <p><strong>Release Date:</strong> {formattedDate}</p>
+                    <p><strong>Publisher:</strong> {props.gameInfo.data.publisher}</p>
+                    <p><strong>Development Studio:</strong> {props.gameInfo.data.development_studio}</p>
+
+                    <p><strong>Summary:</strong></p>
+                    <p>{props.gameInfo.data.summary}</p></div></>
+
+    }
+    return content
+}
+
+
 export default function ReviewPage() {
     const { gameName } = useParams();
 
@@ -101,108 +155,64 @@ export default function ReviewPage() {
 
     let content;
 
-    if (gameInfo.loading) content =
+    content =
+        <>
         <div className="gameInfoBox">
-            <div className="text-center gameInfoLoadingSpinner">
-                <div className="spinner-border" role="status">
-                    <span className="sr-only"></span>
+            < GameInfoComponent gameName={gameName} gameInfo={gameInfo} />
+        {apiPostLoading === STATUS.SUCCESS ? (
+            <p className="submittedText" >Submitted!</p>
+        ) : (
+            <><div className="star-rating">
+                {[...Array(5)].map((star, index) => {
+                    index += 1;
+                    return (
+                        <FaStar
+                            key={index}
+                            className={index <= (hover || rating) ? 'on' : 'off'}
+                            onClick={() => setRating(index)}
+                            onMouseEnter={() => setHover(index)}
+                            onMouseLeave={() => setHover(rating)}
+                            size={30}
+                            color={index <= (hover || rating) ? '#ffd700' : '#e4e5e9'} />
+                    );
+                })}
+            </div>
+                <SubmitButton disabled={apiPostLoading === STATUS.SUBMITTING} value={apiPostLoading === STATUS.SUBMITTING ? "Submitting..." : "Submit"} formSubmitFunction=
+                    {handlePostRequest}
+                    cssClasses={"buttonCenter"} />
+                {apiPostLoading === STATUS.ERROR && <p className="submitErrorMessage" >Sorry something went wrong with submitting your rating, please try again or contact system administrator</p>}
+            </>
+        )}
+        {!comments.loading
+            ? !comments.error
+                ? (<div className="commentsGrid" style={{ padding: 20 }}>
+                    {comments.data.rows.map((item) => (
+                        <><div class="card border-light mb-3 commentCards">
+                            <div class="card-header">01/01/2000</div>
+                            <div class="card-body">
+                                <h5 class="card-title">{item.text}</h5>
+                                <p class="card-text">User1</p>
+                            </div>
+                        </div>
+                        </>
+                    ))}
+                    {commentPage != 1 ? (<button onClick={pageDown} disabled={comments == null}>
+                        Previous
+                    </button>) : (<div></div>)}
+                    <span> Page {commentPage} </span>
+                    {comments.data.nextPage == true ? (<button onClick={pageUp} disabled={comments == null}>
+                        Next
+                    </button>) : (<div></div>)}
+                </div>)
+                : <p className="commentsLoadingErrorMessage">Sorry something went wrong loading comments, please contact the system administrator</p>
+            : <div class="text-center commentLoadingSpinner">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only"></span>
                 </div>
             </div>
-        </div>;
-    else if (gameInfo.error) {
-        content =
-            <div>
-                <p>{gameInfo.error}</p>
-            </div>
-    }
-    else {
-        const timestamp = gameInfo.data.release_date;
-        const formattedDate = new Date(timestamp).toLocaleString("en-US",
-            {
-                month: "short",
-                day: "2-digit",
-                year: "numeric",
-            })
-
-        content =
-            <div className="gameInfoBox">
-                <h1 className='gameTitle'>{gameName}</h1>
-                <div className="gameAverageRating">
-                <h3>Average Rating</h3>
-                    {[...Array(5)].map((star, index, rating) => {
-                        index += 1;
-                        rating = 3;
-                        return (
-                            <FaStar
-                            key={index}
-                            size={30}
-                            className={index <= (hover || rating) ? 'on' : 'off'}
-                            color={index <= (hover || rating) ? '#ffd700' : '#e4e5e9'}/>
-                    );
-                })}</div>
-                <div className='gameInformation'>
-                    <p><strong>Release Date:</strong> {formattedDate}</p>
-                    <p><strong>Publisher:</strong> {gameInfo.data.publisher}</p>
-                    <p><strong>Development Studio:</strong> {gameInfo.data.development_studio}</p>
-
-                    <p><strong>Summary:</strong></p>
-                    <p>{gameInfo.data.summary}</p></div>
-
-                {apiPostLoading === STATUS.SUCCESS ? (
-                    <p className="submittedText" >Submitted!</p>
-                ) : (
-                    <><div className="star-rating">
-                        {[...Array(5)].map((star, index) => {
-                            index += 1;
-                            return (
-                                <FaStar
-                                    key={index}
-                                    className={index <= (hover || rating) ? 'on' : 'off'}
-                                    onClick={() => setRating(index)}
-                                    onMouseEnter={() => setHover(index)}
-                                    onMouseLeave={() => setHover(rating)}
-                                    size={30}
-                                    color={index <= (hover || rating) ? '#ffd700' : '#e4e5e9'} />
-                            );
-                        })}
-                        </div>
-                            <SubmitButton disabled={apiPostLoading === STATUS.SUBMITTING} value={apiPostLoading === STATUS.SUBMITTING ? "Submitting..." : "Submit"} formSubmitFunction=
-                                {handlePostRequest}
-                                cssClasses={"buttonCenter"} />
-                            {apiPostLoading === STATUS.ERROR && <p className="submitErrorMessage" >Sorry something went wrong with submitting your rating, please try again or contact system administrator</p>}
-                    </>
-                )}
-                {!comments.loading
-	                ? !comments.error
-                        ? (<div className="commentsGrid" style={{ padding: 20 }}>
-                            {comments.data.rows.map((item) => (
-                                <><div class="card border-light mb-3 commentCards">
-                                    <div class="card-header">01/01/2000</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title">{item.text}</h5>
-                                        <p class="card-text">User1</p>
-                                    </div>
-                                </div>
-                                </>
-                            ))}
-                            {commentPage != 1 ? (<button onClick={pageDown} disabled={comments == null}>
-                                Previous
-                            </button>) : (<div></div>)}
-                            <span> Page {commentPage} </span>
-                            {comments.data.nextPage == true ? (<button onClick={pageUp} disabled={comments == null}>
-                                Next
-                            </button>) : (<div></div>)}
-                        </div>)
-                        : <p className="commentsLoadingErrorMessage">Sorry something went wrong loading comments, please contact the system administrator</p>
-                    : <div class="text-center commentLoadingSpinner">
-                        <div class="spinner-border" role="status">
-                            <span class="sr-only"></span>
-                        </div>
-                    </div>
-                }
-            </div>
-
-    }
+            }
+        </div>
+        </>
 
     return (
         <Layout>
